5bbb2708b2ef551f287daf3fd28c5d31d388d18e
cherry bharati <cherry.bharati@gmail.com>
2021-01-09 10:24:46 -0500

    first attempt

---
+++ db/connection.js
@@ -0,0 +1,18 @@
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  
+  user: "root",
+  
+  password: "rootroot",
+  database: "employees"
+});
+
+connection.connect();
+
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
